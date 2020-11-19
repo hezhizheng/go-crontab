510edb9e21f2642c8cdb3324f413c2ba6ab908ba
hzz <dexter.ho.cn@gmail.com>
2020-11-20 00:57:16 +0800

    test exec

--- main.go
+++ main.go
@@ -6,17 +6,21 @@ import (
 	"github.com/robfig/cron/v3"
 	"github.com/spf13/viper"
 	"log"
+	"os/exec"
+	"strings"
+	"sync"
 )
 
 type CrontabCmdList struct {
-	Cmd string
+	Cmd     string
 	Crontab string
 }
 
 var ccl []CrontabCmdList
+var mutex sync.Mutex
 
-func init()  {
-	viper.SetConfigType("yaml") // 设置配置文件的类型
+func init() {
+	viper.SetConfigType("json") // 设置配置文件的类型
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".")
 
@@ -39,25 +43,54 @@ func main() {
 	// 从配置文件中读取
 	crontabCmdMap := viper.Get(`app.crontab_cmd`)
 
-
 	jsonStr, e := json.Marshal(crontabCmdMap)
 
 	if e != nil {
-		fmt.Println("333  ",e)
+		fmt.Println("json Marshal error  ", e)
 	}
-
-	fmt.Println("v ",string(jsonStr),crontabCmdMap.([]interface{})[0])
 
 	json.Unmarshal(jsonStr, &ccl)
 
-	fmt.Println("v2 ",crontabCmdMap,ccl,string(jsonStr))
 
+	for _, v := range ccl {
 
-	c.AddFunc("30 * * * *", func() {
-		fmt.Println("Every hour on the half hour")
-	})
+		Crontab := v.Crontab
+		Cmd := v.Cmd
+
+		id, err := c.AddFunc(Crontab, func() {
+
+			p := Cmd
+			pa := Explode(p," ")
+			CommandName := pa[0]
+			mutex.Lock()
+			pa = append(pa[:0], pa[0+1:]...)
+			mutex.Unlock()
+			CommandArg := pa
+
+			f, err := exec.Command(CommandName, CommandArg...).Output()
+
+			if err != nil {
+				log.Println(err.Error())
+			}
+			log.Println(string(f))
+
+		})
+
+		log.Println("定时任务启动", err, id, v.Crontab, v.Cmd)
+	}
 
 	c.Start()
 
-	log.Println("sdfsdfdfdf")
+	log.Println("Start ing ")
+
+	select {}
+}
+
+
+func Explode(delimiter, text string) []string {
+	if len(delimiter) > len(text) {
+		return strings.Split(delimiter, text)
+	} else {
+		return strings.Split(text, delimiter)
+	}
 }
