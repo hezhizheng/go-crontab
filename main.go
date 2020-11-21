b3ace91578799f04e6ae38aee66aa9808753f7c9
hzz <dexter.ho.cn@gmail.com>
2020-11-21 16:29:39 +0800

    init go-crontab

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
 	"github.com/robfig/cron/v3"
-	"github.com/spf13/viper"
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 	"os/exec"
 	"strings"
 	"sync"
@@ -22,40 +22,8 @@ var ccl []CrontabCmdList
 var mutex sync.Mutex
 
 func init() {
-
-	path := "./logs/go.log"
-	/* 日志轮转相关函数
-	`WithLinkName` 为最新的日志建立软连接
-	`WithRotationTime` 设置日志分割的时间，隔多久分割一次
-	WithMaxAge 和 WithRotationCount二者只能设置一个
-	  `WithMaxAge` 设置文件清理前的最长保存时间
-	  `WithRotationCount` 设置文件清理前最多保存的个数
-	*/
-	// 下面配置日志每隔 1 分钟轮转一个新文件，保留最近 3 分钟的日志文件，多余的自动清理掉。
-	writer, _ := rotatelogs.New(
-		path+".%Y%m%d%H%M",
-		rotatelogs.WithLinkName(path),
-		rotatelogs.WithMaxAge(time.Duration(180)*time.Second),
-		rotatelogs.WithRotationTime(time.Duration(60)*time.Second),
-	)
-	log.SetOutput(writer)
-	//log.SetFormatter(&log.JSONFormatter{})
-
-
-	viper.SetConfigType("json") // 设置配置文件的类型
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
-
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			// Config file not found; ignore error if desired
-			log.Println("no such config file")
-		} else {
-			// Config file was found but another error was produced
-			log.Println("read config error")
-		}
-		log.Fatal(err) // 读取配置文件失败致命错误
-	}
+	initLog()
+	initConfig()
 }
 
 func main() {
@@ -74,50 +42,93 @@ func main() {
 	jsonStr, e := json.Marshal(crontabCmdMap)
 
 	if e != nil {
-		fmt.Println("json Marshal error  ", e)
+		log.Error("json Marshal error  ", e)
 	}
 
 	json.Unmarshal(jsonStr, &ccl)
 
+	// 遍历任务
 	for _, v := range ccl {
 
 		Crontab := v.Crontab
 		Cmd := v.Cmd
 
+		// 添加所有配置的 Crontab
 		id, err := c.AddFunc(Crontab, func() {
 
 			p := Cmd
-			pa := Explode(p," ")
+			pa := Explode(p, " ")
 			CommandName := pa[0]
 			mutex.Lock()
 			pa = append(pa[:0], pa[0+1:]...)
 			mutex.Unlock()
 			CommandArg := pa
 
+			// 执行命令
 			f, err := exec.Command(CommandName, CommandArg...).Output()
 
 			if err != nil {
-				log.Println(err.Error())
+				log.Error(err.Error())
 			}
-			log.Println(string(f))
+			log.Println("执行命令：", Cmd, "输出：", string(f))
 
 		})
 
 		if err != nil {
-			log.Println("定时任务启动错误：", err, id, Crontab, Cmd)
-		}else{
-			log.Println("已启动的定时任务： ", id, Crontab, Cmd)
+			fmt.Println("定时任务启动错误：", err, id, Crontab, Cmd)
+		} else {
+			fmt.Println("已启动监听的定时任务： ", id, "表达式：", Crontab, "命令：", Cmd)
 		}
 
 	}
 
 	c.Start()
 
-	log.Println("Start ing ")
+	fmt.Println("Start ing ")
 
 	select {}
 }
 
+func initLog() {
+	log.SetFormatter(&log.JSONFormatter{TimestampFormat: "2006-01-02 15:04:05"})
+
+	path := "./logs/"
+	/* 日志轮转相关函数
+	`WithLinkName` 为最新的日志建立软连接
+	`WithRotationTime` 设置日志分割的时间，隔多久分割一次
+	WithMaxAge 和 WithRotationCount二者只能设置一个
+	  `WithMaxAge 设置文件清理前的最长保存时间`
+	  `WithRotationCount` 设置文件清理前最多保存的个数
+	*/
+	// 下面配置日志每隔 1天 转一个新文件，保留最近 1周 的日志文件，多余的自动清理掉。
+	LinkName := path + "go-crontab.log"
+
+	writer, _ := rotatelogs.New(
+		//path+".%Y%m%d%H%M",
+		path+"go-crontab-%Y-%m-%d.log",
+		rotatelogs.WithLinkName(LinkName),
+		rotatelogs.WithMaxAge(time.Duration(604800)*time.Second),
+		rotatelogs.WithRotationTime(time.Duration(86400)*time.Second),
+	)
+	log.SetOutput(writer)
+}
+
+func initConfig() {
+	viper.SetConfigType("json") // 设置配置文件的类型
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; ignore error if desired
+			log.Error("no such config file")
+		} else {
+			// Config file was found but another error was produced
+			log.Error("read config error")
+		}
+		log.Fatal(err) // 读取配置文件失败致命错误
+	}
+}
 
 func Explode(delimiter, text string) []string {
 	if len(delimiter) > len(text) {
---
+++ readme.md
@@ -0,0 +1,63 @@
+## Go-Crontab
+
+> 基于 golang 的 crontab 定时任务管理器
+
+### features
+- 支持分钟跟秒级
+- 理论上跨平台支持 Windows 、Linux、MacOs
+- 自用test（window下的确没啥好用的定时任务管理器。。。还不如自己搞一个）
+
+### 使用
+
+自定义json配置文件
+```
+# 参数说明
+{
+  # 支持定义多个定时任务，直接在 crontab_cmd 追加对象即可
+  "app": {
+    "model": "s", # 默认 s 秒级 如需使用分钟级改为 m
+    "crontab_cmd": [
+      {
+        "crontab": "0/1 * * * * ?", #crontab 表达式
+        "cmd": "go version" # 要执行的命令 
+      }
+    ]
+  }
+}
+
+# windows 实现 laravel 的任务调度 schedule:run (只支持分钟级别，不能定义秒级的！！！)
+# app.model可以定义为s 但是对应的crontab表达式必须为每分钟运行！可参考 Java(Quartz) 表达式书写
+# 或直接定义app.model 为 m ,则crontab表达式为 "* * * * *"
+{
+  "app": {
+    "model": "s",
+    "crontab_cmd": [
+      {
+        "crontab": "0 0/1 * * * ?",
+        "cmd": "go version"
+      },
+      {
+        "crontab": "0 0/1 * * * ?",
+        "cmd": "php E:\\www\\project\\dexter\\laravel-test-demo\\artisan schedule:run"
+      }
+    ]
+  }
+}
+
+
+```
+
+编译
+```
+go build
+```
+
+
+运行
+- 保证编译的文件与 config.json 在同级目录
+
+- 执行 ./go-crontab.exe (不要关闭终端)
+![free-pic](https://static01.imgkr.com/temp/67bff2160fac49928bd5a8c6f1c6090c.png)
+
+- 执行过程会自动生成log文件(保存一周，会定期清理)
+
